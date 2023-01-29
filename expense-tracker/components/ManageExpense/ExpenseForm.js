3acf90b0428a2b528bbf659dd01d4f319a7e6852
Woong <rlqmatk@naver.com>
2023-01-30 01:12:12 +0900

    Feat: Section08/159

---
+++ expense-tracker/components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,160 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { GlobalStyles } from '../../constants/styles';
+import { getFormattedDate } from '../../util/date';
+import Button from '../UI/Button';
+import Input from './Input';
+
+const ExpenseForm = ({
+  submitButtonLabel,
+  onCancel,
+  onSubmit,
+  defaultValues,
+}) => {
+  const [inputs, setInputs] = useState({
+    amount: {
+      value: defaultValues ? defaultValues.amount.toString() : '',
+      isValid: true,
+    },
+    date: {
+      value: defaultValues ? getFormattedDate(defaultValues.date) : '',
+      isValid: true,
+    },
+    description: {
+      value: defaultValues ? defaultValues.description : '',
+      isValid: true,
+    },
+  });
+
+  const inputChangedHandler = (inputIdentifier, enteredValue) =>
+    setInputs(curInputs => ({
+      ...curInputs,
+      [inputIdentifier]: { value: enteredValue, isValid: true },
+    }));
+
+  const submitHandler = () => {
+    const expenseData = {
+      amount: +inputs.amount.value,
+      date: new Date(inputs.date.value),
+      description: inputs.description.value,
+    };
+
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== 'Invalid Date';
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      setInputs(curInputs => ({
+        amount: { value: curInputs.amount.value, isValid: amountIsValid },
+        date: { value: curInputs.date.value, isValid: dateIsValid },
+        description: {
+          value: curInputs.description.value,
+          isValid: descriptionIsValid,
+        },
+      }));
+      return;
+    }
+
+    onSubmit(expenseData);
+  };
+
+  const formIsInvalid =
+    !inputs.amount.isValid ||
+    !inputs.date.isValid ||
+    !inputs.description.isValid;
+
+  return (
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputRow}>
+        <Input
+          style={styles.rowInput}
+          label="Amount"
+          invalid={!inputs.amount.isValid}
+          textInputConfig={{
+            keyboardType: 'decimal-pad',
+            onChangeText: inputChangedHandler.bind(this, 'amount'),
+            value: inputs.amount.value,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label="Date"
+          invalid={!inputs.date.isValid}
+          textInputConfig={{
+            placeholder: 'YYYY-MM-DD',
+            maxLength: 10,
+            onChangeText: inputChangedHandler.bind(this, 'date'),
+            value: inputs.date.value,
+          }}
+        />
+      </View>
+      <Input
+        label="Description"
+        invalid={!inputs.description.isValid}
+        textInputConfig={{
+          multiline: true,
+          autoCorrect: false,
+          autoCapitalize: 'none',
+          onChangeText: inputChangedHandler.bind(this, 'description'),
+          value: inputs.description.value,
+        }}
+      />
+      {formIsInvalid && (
+        <Text style={styles.errorText}>
+          Invalid input values - Please check your entered data!
+        </Text>
+      )}
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={submitHandler}>
+          {submitButtonLabel}
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 40,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    marginVertical: 24,
+    textAlign: 'center',
+  },
+
+  inputRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  rowInput: {
+    flex: 1,
+  },
+
+  errorText: {
+    textAlign: 'center',
+    color: GlobalStyles.colors.error500,
+    margin: 8,
+  },
+
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+});
+
+export default ExpenseForm;
--- expense-tracker/screens/ManageExpense.js
+++ expense-tracker/screens/ManageExpense.js
@@ -1,7 +1,7 @@
 import { useContext, useLayoutEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
+import ExpenseForm from '../components/ManageExpense/ExpenseForm';
 
-import Button from '../components/UI/Button';
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
 import { ExpensesContext } from '../store/expenses-context';
@@ -11,6 +11,10 @@ const ManageExpense = ({ route, navigation }) => {
 
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
+
+  const selectedExpense = expensesCtx.expenses.find(
+    expense => expense.id === editedExpenseId
+  );
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -27,32 +31,20 @@ const ManageExpense = ({ route, navigation }) => {
     navigation.goBack();
   };
 
-  const confirmHandler = () => {
-    if (isEditing)
-      expensesCtx.updateExpense(editedExpenseId, {
-        description: 'Test!!!!',
-        amount: 19.99,
-        date: new Date('2023-01-27'),
-      });
-    else
-      expensesCtx.addExpense({
-        description: 'Test',
-        amount: 19.99,
-        date: new Date('2023-01-29'),
-      });
+  const confirmHandler = expenseData => {
+    if (isEditing) expensesCtx.updateExpense(editedExpenseId, expenseData);
+    else expensesCtx.addExpense(expenseData);
     navigation.goBack();
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.buttons}>
-        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
-          Cancel
-        </Button>
-        <Button style={styles.button} onPress={confirmHandler}>
-          {isEditing ? 'Update' : 'Add'}
-        </Button>
-      </View>
+      <ExpenseForm
+        submitButtonLabel={isEditing ? 'Update' : 'Add'}
+        onCancel={cancelHandler}
+        onSubmit={confirmHandler}
+        defaultValues={selectedExpense}
+      />
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -72,17 +64,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
-  },
-
-  buttons: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  button: {
-    minWidth: 120,
-    marginHorizontal: 8,
   },
 
   deleteContainer: {
